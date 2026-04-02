5b3ad71a694fdfae14ed3b052667af0006284f43
Artem <159472474+Artem4511@users.noreply.github.com>
2026-04-02 15:58:20 +0500

    Delete Movie List directory

--- Movie List/js/deletFilm.js
+++
@@ -1,7 +0,0 @@
-let delFilmNode = document.querySelector('.js-delFilm');
-
-
-delFilmNode.addEventListener('click', function() {
-  console.log(1)
-  delFilmNode = document.querySelector('.js-delFilm');
-})
