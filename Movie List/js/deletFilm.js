b8a2cd999d7d1697fbf0934d9e3a90e89f03a644
Artem <159472474+Artem4511@users.noreply.github.com>
2026-04-02 15:47:39 +0500

    Upload

---
+++ Movie List/js/deletFilm.js
@@ -0,0 +1,7 @@
+let delFilmNode = document.querySelector('.js-delFilm');
+
+
+delFilmNode.addEventListener('click', function() {
+  console.log(1)
+  delFilmNode = document.querySelector('.js-delFilm');
+})
